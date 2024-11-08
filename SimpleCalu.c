3b6fc3b5938f9d13a172852cb612d163af980580
Sukhneet-Kaur-123 <sukhneetk798@gmail.com>
2024-11-08 13:14:15 +0530

    Create SimpleCalu.c

---
+++ SimpleCalu.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2;
+
+    // Ask the user for input
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    // Perform the operation based on the operator
+    switch(operator) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        default:
+            printf("Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
